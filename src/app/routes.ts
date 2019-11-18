8ec0709eb2ded4396ae3b722275d85e686cb1bc9
Layne Bunker <1bunkers@gmail.com>
2019-11-18 14:14:51 -0800

    End of section 10

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,6 +7,9 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 // This list is order dependent.  the ** needs to be last as a default
 // the empty path adds "" to the children paths.  This just groups paths
@@ -23,7 +26,9 @@ export const appRoutes: Routes = [
       { path: 'members/:id', component: MemberDetailComponent,
         resolve: {user: MemberDetailResolver}},
       { path: 'messages', component: MessagesComponent },
-      { path: 'lists', component: ListsComponent }
+      { path: 'lists', component: ListsComponent },
+      { path: 'member/edit', component: MemberEditComponent, 
+        resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges] }
     ]
   },
   { path: '**', redirectTo: '', pathMatch: 'full' }
