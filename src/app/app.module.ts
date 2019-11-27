5e25c6a46c83404aae33266ed365c17d5673a726
Layne Bunker <1bunkers@gmail.com>
2019-11-27 15:18:20 -0800

    end of sec 16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { ListsResolver } from './_resolvers/lists.resolver';
 
 
 
@@ -99,6 +100,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
       UserService,
       MemberDetailResolver,
       MemberListResolver,
+      ListsResolver,
       MemberEditResolver,
       { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
    ],
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -10,6 +10,7 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { ListsResolver } from './_resolvers/lists.resolver';
 
 // This list is order dependent.  the ** needs to be last as a default
 // the empty path adds "" to the children paths.  This just groups paths
@@ -22,13 +23,14 @@ export const appRoutes: Routes = [
     canActivate: [AuthGuard],
     children: [
       { path: 'members', component: MemberListComponent,
-    resolve: {users: MemberListResolver}},
+          resolve: {users: MemberListResolver}},
       { path: 'members/:id', component: MemberDetailComponent,
-        resolve: {user: MemberDetailResolver}},
+          resolve: {user: MemberDetailResolver}},
       { path: 'messages', component: MessagesComponent },
-      { path: 'lists', component: ListsComponent },
+      { path: 'lists', component: ListsComponent, 
+          resolve: {users: ListsResolver} },
       { path: 'member/edit', component: MemberEditComponent, 
-        resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges] }
+          resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges] }
     ]
   },
   { path: '**', redirectTo: '', pathMatch: 'full' }
